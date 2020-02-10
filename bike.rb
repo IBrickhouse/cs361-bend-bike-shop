bd4a2c9c88d9f53645f9399e0f24a64491324d0b
Isaiah Brickhouse <brickhoi@oregonstate.edu>
2020-02-10 11:45:26 -0800

    Class 10 Exercise Commit

--- bike.rb
+++ bike.rb
@@ -3,37 +3,56 @@
 class Bike
 
   STANDARD_WEIGHT = 200 # lbs
-  MAX_CARGO_ITEMS = 10
 
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
+  attr_accessor :id, :color, :price, :weight,
 
-  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
+  def initialize(id, color, price, weight, storage)
     @id = id
     @color = color
     @price = price
-    @weight = weight
+    @weight = STANDARD_WEIGHT
+    # @rented = rented
+    # @cargo_contents = []
+  end
+
+end
+
+class Rent
+  attr_accessor :rented
+
+  def initialize(rented = false)
     @rented = rented
-    @cargo_contents = []
   end
 
   def rent!
     self.rented = true
   end
 
+end
+
+class Cargo
+
+  attr_accessor :cargo_contents
+
+  MAX_ITEMS = 10
+
+  def initialize(cargo_contents)
+    @cargo_contents = []
+  end
+
   def add_cargo(item)
-    self.cargo_contents << item
+    self.cargo.add(item)
   end
 
   def remove_cargo(item)
-    self.cargo_contents.remove(item)
+    self.cargo.remove(item)
   end
 
-  def pannier_capacity
-    MAX_CARGO_ITEMS
+  def capacity
+    MAX_ITEMS
   end
 
-  def pannier_remaining_capacity
+  def remaining_capacity
     MAX_CARGO_ITEMS - self.cargo_contents.size
   end
-
 end
